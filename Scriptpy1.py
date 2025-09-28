e5f6610f0a413424e0e662265179884adeefe628
Yoan WALLOIS <yoan_wallois@protonmail.com>
2025-09-28 12:36:39 +0200

    Install python .venv

---
+++ Scriptpy1.py
@@ -0,0 +1,11 @@
+"""
+Creadt by: Yoan Wallois
+Date: 2024-09-28
+Purpose: Simple script to convert a database of climate extreme events to climate forecast events.
+"""
+#%% Imports
+import pandas as pd
+import numpy as np 
+from Pathlib import Path
+
+# %%
