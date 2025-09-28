e9a842d85c699b299f94d968f0e7f5444c997937
Yoan WALLOIS <yoan_wallois@protonmail.com>
2025-09-28 19:41:12 +0200

    import data

--- Scriptpy1.py
+++ Scriptpy1.py
@@ -6,6 +6,12 @@ Purpose: Simple script to convert a database of climate extreme events to climat
 #%% Imports
 import pandas as pd
 import numpy as np 
-from Pathlib import Path
+from pathlib import Path
 
 # %%
+
+
+with open(Path().joinpath('global_climate_events_economic_impact_2020_2025.csv'), 'r', encoding='utf-8') as f:
+    ExtremeEvents = pd.read_csv(f, sep=',')
+
+EMDAT = pd.read_excel(Path().joinpath("public_emdat_custom_request_2025-09-28_8317efb3-ee1c-4a60-8ccd-023043de642f.xlsx"), engine='openpyxl')
